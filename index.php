f59ec375ae329d0793f800557ee99f4d9836ce3d
Pontus Sundén <pontus@psu.se>
2011-08-21 20:35:26 +0200

    added stylesheet

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"> 
+  <head> 
+
+		<title></title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+    <link rel="icon" type="image/png" size="129x129" href="./icon.png" />    
+    <link rel="stylesheet" type="text/css" href="./style.css" /> 		
+
+    <style type="text/css">
+
+      BODY {
+      
+      }
+    
+
+    </style>
+
+	</head>
+	<body>
+	
+	<h1><?php echo "Hello World!"; ?></h1>
+	Developing!
+	
+	
+    <!-- google -->
+    <script type="text/javascript">
+/*
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-XXXXX']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+*/
+    </script>
+    <!-- google -->		
+
+	</body>
+</html>
